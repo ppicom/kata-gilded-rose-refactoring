900730aee943ff83cc496d7a51c2bad04c4d200d
Pere <perepicomuntaner@gmail.com>
2023-01-31 07:57:58 +0100

    Refactor to make change easy.

--- internal/gildedrose/gildedrose.go
+++ internal/gildedrose/gildedrose.go
@@ -3,51 +3,42 @@ package gildedrose
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
+		if items[i].Name == "Sulfuras, Hand of Ragnaros" {
+			items[i].SellIn -= 1
+			continue
+		}
+
+		if items[i].Name == "Aged Brie" {
 			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
+				items[i].Quality += 1
 			}
+			items[i].SellIn -= 1
+			continue
 		}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
+		if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+			var qualityDelta int = 1
+			switch {
+			case items[i].SellIn < 1:
+				qualityDelta = -items[i].Quality
+			case items[i].SellIn <= 5:
+				qualityDelta = 3
+			case items[i].SellIn <= 10:
+				qualityDelta = 2
+			}
+			items[i].Quality += qualityDelta
+			items[i].SellIn -= 1
+			continue
 		}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
+		items[i].SellIn -= 1
+
+		if items[i].Quality > 0 {
+			var qualityDecrease = 1
+			if items[i].SellIn < 1 {
+				qualityDecrease = 2
 			}
+			items[i].Quality -= qualityDecrease
 		}
 	}
-
 }
