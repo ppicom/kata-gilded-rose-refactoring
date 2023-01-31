6b557645ead08c25ec23ee2be4cfd681d56b94a3
Pere <perepicomuntaner@gmail.com>
2023-01-31 07:58:02 +0100

    Extract functions refactor.

--- internal/gildedrose/gildedrose.go
+++ internal/gildedrose/gildedrose.go
@@ -6,55 +6,75 @@ func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
 		if items[i].Name == "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn -= 1
+			transformSulfuras(items[i])
 			continue
 		}
 
 		if items[i].Name == "Aged Brie" {
-			if items[i].Quality < 50 {
-				items[i].Quality += 1
-			}
-			items[i].SellIn -= 1
+			transformBrie(items[i])
 			continue
 		}
 
 		if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-			var qualityDelta int = 1
-			switch {
-			case items[i].SellIn < 1:
-				qualityDelta = -items[i].Quality
-			case items[i].SellIn <= 5:
-				qualityDelta = 3
-			case items[i].SellIn <= 10:
-				qualityDelta = 2
-			}
-			items[i].Quality += qualityDelta
-			items[i].SellIn -= 1
+			transformBasckstagePasses(items[i])
 			continue
 		}
 
 		if strings.HasPrefix(items[i].Name, "Conjured") {
-			if items[i].Quality > 0 {
-				var qualityDecrease = 2
-				if items[i].SellIn < 1 {
-					qualityDecrease = 4
-				}
-				items[i].Quality -= qualityDecrease
-			}
-
-			items[i].SellIn -= 1
+			transformConjured(items[i])
 			continue
 		}
 
-		if items[i].Quality > 0 {
-			var qualityDecrease = 1
-			if items[i].SellIn < 1 {
-				qualityDecrease = 2
-			}
-			items[i].Quality -= qualityDecrease
-		}
-
-		items[i].SellIn -= 1
+		transformRegularItem(items[i])
 
 	}
+}
+
+func transformBasckstagePasses(item *Item) {
+	var qualityDelta int = 1
+	switch {
+	case item.SellIn < 1:
+		qualityDelta = -item.Quality
+	case item.SellIn <= 5:
+		qualityDelta = 3
+	case item.SellIn <= 10:
+		qualityDelta = 2
+	}
+	item.Quality += qualityDelta
+	item.SellIn -= 1
+}
+
+func transformSulfuras(item *Item) {
+	item.SellIn -= 1
+}
+
+func transformBrie(item *Item) {
+	if item.Quality < 50 {
+		item.Quality += 1
+	}
+	item.SellIn -= 1
+}
+
+func transformRegularItem(item *Item) {
+	if item.Quality > 0 {
+		var qualityDecrease = 1
+		if item.SellIn < 1 {
+			qualityDecrease = 2
+		}
+		item.Quality -= qualityDecrease
+	}
+
+	item.SellIn -= 1
+}
+
+func transformConjured(item *Item) {
+	if item.Quality > 0 {
+		var qualityDecrease = 2
+		if item.SellIn < 1 {
+			qualityDecrease = 4
+		}
+		item.Quality -= qualityDecrease
+	}
+
+	item.SellIn -= 1
 }
