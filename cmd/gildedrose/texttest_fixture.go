1dd09b94081285f424a860637c4ffe32591b1712
Pere <perepicomuntaner@gmail.com>
2023-01-31 07:57:52 +0100

    Explicitly name Item fields.

--- cmd/gildedrose/texttest_fixture.go
+++ cmd/gildedrose/texttest_fixture.go
@@ -12,15 +12,15 @@ func main() {
 	fmt.Println("OMGHAI!")
 
 	var items = []*gildedrose.Item{
-		{"+5 Dexterity Vest", 10, 20},
-		{"Aged Brie", 2, 0},
-		{"Elixir of the Mongoose", 5, 7},
-		{"Sulfuras, Hand of Ragnaros", 0, 80},
-		{"Sulfuras, Hand of Ragnaros", -1, 80},
-		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
-		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
-		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
-		{"Conjured Mana Cake", 3, 6}, // <-- :O
+		{Name: "+5 Dexterity Vest", SellIn: 10, Quality: 20},
+		{Name: "Aged Brie", SellIn: 2, Quality: 0},
+		{Name: "Elixir of the Mongoose", SellIn: 5, Quality: 7},
+		{Name: "Sulfuras, Hand of Ragnaros", SellIn: 0, Quality: 80},
+		{Name: "Sulfuras, Hand of Ragnaros", SellIn: -1, Quality: 80},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 15, Quality: 20},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 10, Quality: 49},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 5, Quality: 49},
+		{Name: "Conjured Mana Cake", SellIn: 3, Quality: 6}, // <-- :O
 	}
 
 	days := 2
